9b3a1e807ad66a1fb8147b6b8e1feb0917b861ba
sergey-komar <kursev81@yandex.ru>
2024-07-21 13:25:33 +0300

    2page desctop mobile

--- app/js/main.js
+++ app/js/main.js
@@ -6,14 +6,51 @@ $(function () {
         dots: true
      });
 
+
      $('.product-slider').slick({
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+                {
+                  breakpoint: 1250,
+                  settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                  },
+                },
+                {
+                breakpoint: 900,
+                settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                },
+                },
+                {
+                breakpoint: 576,
+                settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                },
+                },
+               
+              ],
      });
 
      $('.slider-stock__inner').slick({
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+                {
+                  breakpoint: 650,
+                  settings: {
+                       arrows: false,
+                       dots: true
+                  },
+                },
+               
+               
+              ],
+        
      });
 
 
@@ -38,10 +75,25 @@ $(function () {
 
 
 window.addEventListener('DOMContentLoaded',() => {
-        const mobile = document.querySelector(".header-box__icon");
+        const menu = document.querySelector(".menu");
+        const mobile = document.querySelector(".nav-icon");
+        const itemDropdown = document.querySelector('.menu__list-dropdown');
+        const menuVertical = document.querySelector('.menu-vertical');
+        const verticalArrows = document.querySelector('.menu-vertical__arrows');
       
         mobile.addEventListener("click", function () {
-          this.classList.toggle("header-box__icon--active");
-        
+         document.body.classList.toggle('mobile-class');
+          this.classList.toggle("nav-icon--active");
+          menu.classList.toggle("nav--active");
+          document.body.classList.toggle('overflow');
         });
+
+        itemDropdown.addEventListener('click',() => {
+                menuVertical.classList.toggle('opacity-dropdown');
+                mobile.classList.toggle('nav-icon-zindex');
+        })
+        verticalArrows.addEventListener('click', () => {
+                menuVertical.classList.remove('opacity-dropdown');
+                mobile.classList.remove('nav-icon-zindex');
+        })
 })
