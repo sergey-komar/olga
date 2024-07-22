54c64bb07d59b0ebe75fa5641ba345a89baef5b1
sergey-komar <kursev81@yandex.ru>
2024-07-22 19:16:39 +0300

    5page

--- app/js/main.js
+++ app/js/main.js
@@ -69,7 +69,12 @@ $(function () {
                 }
         }
         $button.parent().find('input').val(newVal);
-});
+        });
+
+
+        $('.questions-accardion__btn').on('click', function(){
+                $(this).next().slideToggle(500); 
+             });
 })
 
 
@@ -80,13 +85,18 @@ window.addEventListener('DOMContentLoaded',() => {
         const itemDropdown = document.querySelector('.menu__list-dropdown');
         const menuVertical = document.querySelector('.menu-vertical');
         const verticalArrows = document.querySelector('.menu-vertical__arrows');
+        // const test = calcScroll();
       
         mobile.addEventListener("click", function () {
          document.body.classList.toggle('mobile-class');
           this.classList.toggle("nav-icon--active");
           menu.classList.toggle("nav--active");
           document.body.classList.toggle('overflow');
+        //   document.body.style.marginRight = `${test}px`;
+        document.body.classList.toggle('w');
         });
+       
+       
 
         itemDropdown.addEventListener('click',() => {
                 menuVertical.classList.toggle('opacity-dropdown');
@@ -96,4 +106,41 @@ window.addEventListener('DOMContentLoaded',() => {
                 menuVertical.classList.remove('opacity-dropdown');
                 mobile.classList.remove('nav-icon-zindex');
         })
+
+
+        // function calcScroll(){
+        // let div = document.createElement('div');
+        // div.style.width = '50px';
+        // div.style.height = '50px';
+        // div.style.overflowY = 'scroll';
+        // div.style.visibility = 'hidden';
+        
+        // document.body.append(div);
+
+        // let scrollWidth = div.offsetWidth - div.clientWidth;
+        
+        // div.remove();
+        
+        // return scrollWidth;
+        // }
+
+        const newsHidden = document.querySelector('.news-hidden');
+        const newsBtn = document.querySelector('.news__btn');
+        if(newsBtn){
+                newsBtn.addEventListener('click', () => {
+                        newsHidden.classList.add('news-show');
+                })
+        }
+       
+
+
+        const accardionBtn = document.querySelectorAll('.questions-accardion__btn');
+                accardionBtn.forEach(item => {
+                item.addEventListener('click', () => {
+                        item.classList.toggle('open')
+                });
+        });
+
+
+
 })
